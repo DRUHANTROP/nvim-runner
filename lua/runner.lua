a13607a65898aa9d48bb28ecb08a03a6aa3e82a5
DRUHANTROP <andrewwest751@gmail.com>
2024-12-27 20:47:36 +0300

    lua can no longer find the executable for some reason

--- lua/runner.lua
+++ lua/runner.lua
@@ -1,5 +1,5 @@
 local M = {}
-local output = 69
+local output, job, win = 69, 420, 1337
 
 local handle_stdout = function(_, data)
 	if data then
@@ -7,18 +7,33 @@ local handle_stdout = function(_, data)
 	end
 end
 
-local run_lua = function()
+local run_file = function(command)
 	vim.api.nvim_buf_set_lines(output, 0, -1, false, { "OUTPUT:" })
-	local job = vim.fn.jobstart({ "lua", vim.api.nvim_buf_get_name(0) }, {
-		stdout_buffered = true,
+	job = vim.fn.jobstart({ command, vim.api.nvim_buf_get_name(0) }, {
 		on_stdout = handle_stdout,
 		on_stderr = handle_stdout,
-		-- on_exit = handle_stdout(nil, { "-EXIT-" }),
 	})
 end
 
+local create_window = function()
+	local width, height = vim.api.nvim_win_get_width(0), vim.api.nvim_win_get_height(0)
+	local buf = vim.api.nvim_create_buf(false, true)
+	local opts = {
+		relative = "editor",
+		width = math.floor(width / 2),
+		height = height,
+		col = math.floor(width / 4),
+		row = 1,
+		anchor = "NW",
+		style = "minimal",
+	}
+	win = vim.api.nvim_open_win(buf, false, opts)
+	vim.api.nvim_set_current_win(win)
+	output = buf
+end
+
 local runners = {
-	["lua"] = { is_interpreted = true, runner_name = "lua", run = run_lua },
+	["lua"] = { is_interpreted = true, command = "lua", run = run_file },
 }
 
 M.run_current_cwd = function()
@@ -27,24 +42,17 @@ M.run_current_cwd = function()
 		print("You didn't teach me how to run it")
 		return
 	end
-	local width, height = vim.api.nvim_win_get_width(0), vim.api.nvim_win_get_height(0)
-	local buf = vim.api.nvim_create_buf(false, true)
-	local opts = {
-		relative = "editor",
-		width = math.floor(width / 2),
-		height = height,
-		col = math.floor(width / 2),
-		row = 1,
-		anchor = "NW",
-		style = "minimal",
-	}
-	vim.api.nvim_open_win(buf, false, opts)
-	output = buf
-	runner.run()
+	create_window()
+	runner.run(runner.command)
 end
 
 M.setup = function(opts)
 	print("hello !", opts)
 end
+
+M.close = function()
+	vim.fn.jobstop(job)
+end
+
 print("lol")
 return M
