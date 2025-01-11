7d4770d7c96099122b82c8e637f86d3ad30f96c6
DRUHANTROP <andrewwest751@gmail.com>
2025-01-11 13:24:27 +0300

    fixed the bug that caused runner to only run first opened file

--- lua/runner.lua
+++ lua/runner.lua
@@ -1,15 +1,17 @@
 local M = {}
 local kind = { SINGLE_FILE = 1, CWD = 2, CUSTOM = 3 }
-local target = { [1] = vim.fn.expand("%"), [2] = vim.fn.getcwd(), [3] = {} }
-local output, job, win = 69, 420, 1337
+local target = {
+	[1] = function()
+		return vim.fn.expand("%")
+	end,
+	[2] = vim.fn.getcwd,
+	[3] = function() end,
+}
+
+local output, job, win = -1, -1, -1
 
 local handle_stdout = function(_, data)
 	if data then
-		print(data[1])
-		if vim.trim(data[1]) == "[H[2J" then
-			vim.api.nvim_buf_set_lines(output, 0, -1, false, { "OUTPUT: " })
-			return
-		end
 		vim.api.nvim_buf_set_lines(output, -1, -1, false, data)
 	end
 end
@@ -17,7 +19,8 @@ end
 local run = function(runner)
 	vim.api.nvim_buf_set_lines(output, 0, -1, false, { "OUTPUT: " })
 
-	job = vim.fn.jobstart({ runner.command, target[runner.kind] }, {
+	-- local path = target[runner.kind]()
+	job = vim.fn.jobstart({ runner.command, target[runner.kind]() }, {
 		on_stdout = handle_stdout,
 		on_stderr = handle_stdout,
 	})
@@ -39,8 +42,20 @@ local runners = {
 	["javascript"] = { kind = kind.SINGLE_FILE, command = "node" },
 }
 
+local close = function()
+	vim.fn.jobstop(job)
+	vim.api.nvim_win_close(win, true)
+	vim.api.nvim_buf_delete(output, { force = true })
+end
+
 local run_current_cwd = function()
+	if vim.api.nvim_buf_is_loaded(output) then
+		close()
+		return
+	end
+
 	local runner = runners[vim.bo.filetype]
+
 	if not runner then
 		print("You didn't teach me how to run it")
 		return
@@ -49,25 +64,13 @@ local run_current_cwd = function()
 	run(runner)
 end
 
-local close = function()
-	vim.fn.jobstop(job)
-	if vim.api.nvim_buf_is_loaded(output) then
-		vim.api.nvim_win_close(win, true)
-		vim.api.nvim_buf_delete(output, { force = true })
-	end
-end
-
 M.setup = function(opts)
-	vim.api.nvim_create_user_command("FreeRun", function()
+	vim.api.nvim_create_user_command("ToggleRun", function()
 		run_current_cwd()
 	end, {})
 
-	vim.api.nvim_create_user_command("FreeStop", function()
-		close()
-	end, {})
-
-	if opts.runners then
-		runners.insert(opts.runners)
+	for k, v in pairs(opts.runners) do
+		runners[k] = v
 	end
 end
 
